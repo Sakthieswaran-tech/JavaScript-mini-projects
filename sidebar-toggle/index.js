15e905d74f747eee1c96714242cbb2b6853f007d
Sakthieswaran-tech <sakthichunkz17@gmail.com>
2022-05-05 20:01:57 +0530

    side-bar toggle

---
+++ sidebar-toggle/index.js
@@ -0,0 +1,11 @@
+const togBtn=document.querySelector(".sidebar-toggle");
+const sidebar=document.querySelector(".sidebar");
+const closeBtn=document.querySelector(".close-btn");
+
+togBtn.addEventListener("click",()=>{
+    sidebar.classList.toggle("show-sidebar");
+})
+
+closeBtn.addEventListener("click",()=>{
+    sidebar.classList.remove("show-sidebar")
+})
